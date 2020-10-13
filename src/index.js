d0b100dd442a4933834d916cfb752dbabdaa1638
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2020-10-13 16:13:33 +0300

    feat: add human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const digit = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const tensDigit = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let numberString = number.toString();
+
+    if (numberString == 0) {
+        return "zero";
+    }
+
+    if (numberString < 20) {
+        return digit[numberString];
+    }
+
+    if (numberString < 100) {
+        return `${tensDigit[numberString[0]]} ${digit[numberString[1]]}`.trim();
+    }
+
+    if (numberString >= 100) {
+        let numberMoreTenString =
+            numberString[1] + numberString[2] >= 10
+                ? numberString[1] + numberString[2]
+                : numberString[2];
+
+        let numberLessTwentyString =
+            numberMoreTenString < 20
+                ? `${digit[numberMoreTenString]}`
+                : `${tensDigit[numberMoreTenString[0]]} ${
+                      digit[numberMoreTenString[1]]
+                  }`;
+
+        return numberString[1] === 0 && numberString[2] === 0
+            ? `${digit[numberString[0]]}hundred`.trim()
+            : `${digit[numberString[0]]} hundred ${numberLessTwentyString}`.trim();
+    }
+};
